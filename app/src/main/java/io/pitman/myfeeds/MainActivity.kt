a8a015c124dbd178d669147fc176f9476a6121c8
Mark Pitman <mapitman@users.noreply.github.com>
2026-07-14 14:14:19 -0700

    Scaffold Android project (#32)
    Compose empty-activity template with Kotlin DSL and version catalog
    (minSdk 31, compileSdk/targetSdk 36, package io.pitman.myfeeds),
    Material 3 theme seeded with the green MyFeeds brand color, placeholder
    launcher icon, Gradle wrapper, mise-managed Java/Gradle toolchain, and
    a CI build stub.

---
+++ app/src/main/java/io/pitman/myfeeds/MainActivity.kt
@@ -0,0 +1,41 @@
+package io.pitman.myfeeds
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.pitman.myfeeds.ui.theme.MyFeedsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyFeedsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(name = "MyFeeds", modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyFeedsTheme {
+        Greeting("MyFeeds")
+    }
+}
---
+++ app/src/main/java/io/pitman/myfeeds/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package io.pitman.myfeeds.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val MyFeedsGreen80 = Color(0xFFA5D6A7)
+val MyFeedsGreenGrey80 = Color(0xFFC8E6C9)
+val MyFeedsAccent80 = Color(0xFFB2DFDB)
+
+val MyFeedsGreen40 = Color(0xFF2E7D32)
+val MyFeedsGreenGrey40 = Color(0xFF388E3C)
+val MyFeedsAccent40 = Color(0xFF00695C)
